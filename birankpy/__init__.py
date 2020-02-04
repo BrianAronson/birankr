56b5ce3edaad12f8afdb023c1ccaf7d237b6923f
yangkc <yangkc@iu.edu>
2020-02-04 01:19:42 -0500

    add generate_birank method

--- birankpy/__init__.py
+++ birankpy/__init__.py
@@ -221,3 +221,16 @@ class BipartiteNetwork:
         bottom_df = self.df.groupby(self.bottom_col)[self.top_col].nunique()
         bottom_df = bottom_df.to_frame(name='degree').reset_index()
         return top_df, bottom_df
+
+    def generate_birank(self, **kwargs):
+        """
+        This method performs BiRank algorithm on the bipartite network and
+        returns the ranking values for both the top nodes and bottom nodes.
+        """
+        d, p = birank(self.W, **kwargs)
+        self.top_ids[self.top_col + '_birank'] = d
+        self.bottom_ids[self.bottom_col + '_birank'] = p
+        return (
+            self.top_ids[[self.top_col, self.top_col + '_birank']],
+            self.bottom_ids[[self.bottom_col, self.bottom_col + '_birank']]
+        )

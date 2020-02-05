dc7da8e1980ad6034aa1f890cca43e26957212aa
yangkc <yangkc@iu.edu>
2020-02-05 17:29:02 -0500

    better setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,15 @@ from distutils.core import setup
 setup(name='birankpy',
       version='0.1',
       description = 'Ranking nodes in bipartite networks',
-      author = 'Kaicheng Yang <yangkc@iu.edu>',
+      author = 'Kaicheng Yang'
+      author_email = 'yangkc@iu.edu',
+      url="https://github.com/BrianAronson/birankr",
       license = 'MIT',
+      install_requires=[
+          'networkx',
+          'pandas',
+          'numpy',
+          'scipy'
+      ],
       packages = ['birankpy']
 )

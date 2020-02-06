84d74cb522f35a60b128511ad929f275098b0e5f
yangkc <yangkc@iu.edu>
2020-02-05 21:51:59 -0500

    fix the document for pypi

--- setup.py
+++ setup.py
@@ -5,9 +5,10 @@ with open("pypi_readme.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='birankpy',
-      version='0.1',
+      version='0.1.1',
       description = 'Ranking nodes in bipartite networks with efficiency and flexibility',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author = 'Kaicheng Yang',
       author_email = 'yangkc@iu.edu',
       url="https://github.com/BrianAronson/birankr",

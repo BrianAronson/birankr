57ad8530ffe6be22877418b8288aec41c24451c1
BrianAronson <bdaronson@gmail.com>
2020-02-06 18:15:08 -0500

    Fixed broken test

--- tests/testthat/test-01_project_to_one_mode.R
+++ tests/testthat/test-01_project_to_one_mode.R
@@ -3,7 +3,7 @@ test_that("project_to_one_mode produces square Matrix", {
                             j = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), x = 1
   )
   mat <- project_to_one_mode(adj_mat = my_matrix, mode = "rows")
-  expect_equal(dim(mat)[1] == dim(mat)[2])
+  expect_equal(dim(mat)[1], dim(mat)[2])
 })
 
 
